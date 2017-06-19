794c10c6fc85acf94bdad6f34deca7b829d80d88
“lukemccrae” <lukemccrae@gmail.com>
2017-06-18 21:57:37 -0600

    map with directions is displaying, need to get text directions to display

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var howMany;
 var destinations = [];
 var home;
 var current;
-var string = 'https://maps.googleapis.com/maps/api/directions/json?mode=bicycling&origin=38.8713700,-104.8221310&destination=38.9091780,-104.7828480&key=AIzaSyB6mjYhp5ca_RPpOdHu_Ul7E-YY6BYzmms'
+var path;
+var waypoint = []
+var directionsDisplay = new google.maps.DirectionsRenderer;
+var directionsService = new google.maps.DirectionsService;
 
 function buttonClick() {
     howMany = parseInt(document.getElementById("number").value)
@@ -10,12 +13,9 @@ function buttonClick() {
 }
 
 function findCurrent() {
-    console.log(howMany);
     navigator.geolocation.getCurrentPosition(function(position) {
         $.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&key=AIzaSyB6mjYhp5ca_RPpOdHu_Ul7E-YY6BYzmms')
-            .done(function(data) {
-                console.log(data);
-            })
+            .done(function(data) {})
             .fail(function(error) {
                 console.log(error);
             })
@@ -28,7 +28,6 @@ function findCurrent() {
 }
 
 function findLocations(location) {
-    console.log(location);
     var position = new google.maps.LatLng(location.lat, location.lng);
     map = new google.maps.Map(document.getElementById('map'), {
         center: position,
@@ -36,7 +35,7 @@ function findLocations(location) {
     });
     var request = {
         location: position,
-        radius: '2000',
+        radius: '4000',
         types: ['bar']
     };
     service = new google.maps.places.PlacesService(map);
@@ -53,12 +52,24 @@ function addLocation(results, status) {
         }
         if (alreadyAdded(currentLoc.name)) {
             destinations.push(currentLoc)
+            //string method of pushing to waypoint
+            waypoint.push({
+                location: currentLatLng.lat + ',' + currentLatLng.lng
+            })
+            console.log(currentLoc.name);
+            //variable method of adding waypoints
+            // waypoint.push(currentLatLng)
+
+            //google maps object waypoints
+            // waypoints.push(new google.maps.LatLng(currentLatLng.lat, currentLatLng.lng))
             howMany += -1
-            console.log(destinations);
             searchAgain(currentLatLng)
         } else {
             searchAgain(currentLatLng)
         }
+    }
+    if (!results || !howMany) {
+        finishedSearching(currentLatLng)
     }
 }
 
@@ -71,9 +82,60 @@ function searchAgain(currentLatLng) {
 function alreadyAdded(loc) {
     for (var i = 0; i < destinations.length; i++) {
         if (destinations[i].name === loc) {
-            console.log('duplicate');
             return false;
         }
     }
     return true;
+}
+
+function finishedSearching(end) {
+    path = `https://maps.googleapis.com/maps/api/directions/json?mode=bicycling&origin=${home.lat},${home.lng}&destination=${home.lat},${home.lng}&waypoint=${waypoint}&key=AIzaSyB6mjYhp5ca_RPpOdHu_Ul7E-YY6BYzmms`
+    // $.get(path).done(function(data) {
+    //     console.log(data);
+    // })
+    initialize()
+}
+
+function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var chicago = new google.maps.LatLng(home.lat, home.lng);
+    var mapOptions = {
+        zoom: 15,
+        center: chicago
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay)
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    var start = home
+    var end = home
+    directionsService.route({
+        origin: start,
+        destination: end,
+        travelMode: 'BICYCLING',
+        waypoints: waypoint,
+        optimizeWaypoints: true
+    }, function(response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+}
+
+function calcRoute() {
+    var request = {
+        origin: start,
+        destination: start,
+        travelMode: 'BICYCLING',
+        waypoints: waypoints
+    };
+    directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+            directionsDisplay.setDirections(result);
+        }
+    });
 }
